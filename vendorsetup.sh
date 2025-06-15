d566705455ea0c2c9e6fc9c7274dd86dc6b3f170
NoMI <105900291+Jayy-Dev@users.noreply.github.com>
2025-06-15 16:55:53 +0800

    Removed depreciated vars

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -37,9 +37,8 @@ fi
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 
     # Version & Variant
-    export FOX_VERSION="R12.1"
+    export FOX_MAINTAINER_PATCH_VERSION="2"
     export FOX_VARIANT="A14"
-    export FOX_BUILD_TYPE="Stable"
 
     # OF Addons
     export FOX_ENABLE_APP_MANAGER=1
