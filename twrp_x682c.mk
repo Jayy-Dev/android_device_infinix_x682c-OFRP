bd2d5556ee2b2f54c35f70c77279c87cf4862774
NoMI <105900291+Jayy-Dev@users.noreply.github.com>
2025-06-15 16:19:52 +0800

    Update twrp_x682c.mk

--- twrp_x682c.mk
+++ twrp_x682c.mk
@@ -1,3 +1,10 @@
 PRODUCT_RELEASE_NAME := x682c
 DEVICE_PATH := device/infinix/x682c
 $(call inherit-product-if-exists, $(DEVICE_PATH)/fox_$(PRODUCT_RELEASE_NAME).mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := $(PRODUCT_RELEASE_NAME)
+PRODUCT_NAME := twrp_$(PRODUCT_RELEASE_NAME)
+PRODUCT_BRAND := infinix
+PRODUCT_MODEL := infinix x682c
+PRODUCT_MANUFACTURER := infinix
